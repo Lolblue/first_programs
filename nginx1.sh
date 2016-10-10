8208d4dfab6b70b8c192a69f54d4e16db812828b
Lola <2ndtime3rdtime@gmail.com>
2016-10-09 22:32:34 -0400

    Add nginx1.sh

--- nginx1.sh
+++ nginx1.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
 # Install of Nginx
-fileD="/etc/nginx/sites-available/default
-http_upgrade="one"
-host=two
+kibana_conf="/etc/nginx/conf.d/kibana.conf"
+systemctl stop httpd
+systemctl disable httpd
+yum remove nginx httpd-tools
 
-sudo yum install nginx apache2-utils
-
-# wait for command to complete
-sleep 10
+yum -y install epel-release
+yum -y install nginx httpd-tools
 
 # set user/password in HTTP password file.  Create the password file if it does not exist
 htpasswd -b -c /etc/nginx/htpasswd.users kibanaadmin Top_labmix
 
-# empty contents of file, then write new lines to file
-truncate -s 0 /etc/nginx/sites-available/default
-fileD="/etc/nginx/htpasswd.users"
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.confbk
+sed '/server {/,$ d' /etc/nginx/nginx.conf | tee /etc/nginx/nginx_conf7
+echo "}" >> /etc/nginx/nginx_conf7
+cp /etc/nginx/nginx_conf7 /etc/nginx/nginx.conf
+touch /etc/nginx/conf.d/kibana.conf
+# echo contents into file
+ 
+echo "server {" > $kibana_conf
+echo -e '\t listen 80;' >> $kibana_conf
+echo -e '\t server_name localhost;' >> $kibana_conf
+echo -e '\t auth_basic "Restricted Access";' >> $kibana_conf
+echo -e '\t auth_basic_user_file /etc/nginx/htpasswd.users;' >> $kibana_conf
+echo -e '\t location / {' >> $kibana_conf
+echo -e '\t \t proxy_pass http://localhost:5601;' >> $kibana_conf
+echo -e '\t \t proxy_http_version 1.1;' >> $kibana_conf
+echo -e '\t \t proxy_set_header Upgrade $http_upgrade;' >> $kibana_conf
+echo -e '\t \t proxy_set_header Connection 'upgrade';' >> $kibana_conf
+echo -e '\t \t proxy_set_header Host $host;' >> $kibana_conf
+echo -e '\t \t proxy_cache_bypass $http_upgrade;' >> $kibana_conf
+echo -e '\t }' >> $kibana_conf
+echo -e '}' >> $kibana_conf
+ 
+systemctl start nginx
+systemctl enable nginx
 
-echo "server {" > $fileD
-echo -e '\t listen 80;' >> $fileD
-echo -e '\t server_name example.com;' >> $fileD
-echo -e '\t auth_basic "Restricted Access";' >> $fileD
-echo -e '\t auth_basic_user_file /etc/nginx/htpasswd.users;' >> $fileD
-echo -e '\t location / {' >> $fileD
-echo -e '\t \t proxy_pass http://localhost:5601;' >> $fileD
-echo -e '\t \t proxy_http_version 1.1;' >> $fileD
-echo -e '\t \t proxy_set_header Upgrade $http_upgrade;' >> $fileD
-echo -e '\t \t proxy_set_header Connection 'upgrade';' >> $fileD
-echo -e '\t \t proxy_set_header Host $host;' >> $fileD
-echo -e '\t \t proxy_cache_bypass $http_upgrade;' >> $fileD
-echo -e '\t }' >> $fileD
-echo -e '}' >> $fileD
+exit
 
-Sed -i 's/example.com/myserver/g' /etc/nginx/sites-available/default
 
-sudo servie nginx restart
